5fa485b826698278c8ac2499c8e708c5d8712ccf
smndiaye <serigne.ndiaye@monstar-lab.com>
2016-02-22 14:01:25 +0900

    Full Title Added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Language Learning"
+    assert_select "title", "Language Learning"
   end
 
   test "should get help" do
