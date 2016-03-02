a3e29ba0153cc99fd124bc518ec479c0e8ee67db
smndiaye <serigne.ndiaye@monstar-lab.com>
2016-03-02 11:20:18 +0900

    SQL inj added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
   def self.search(search)
-    #where("name LIKE ?", "%#{search}%") #safe
-    #find(:all, :conditions => ['name LIKE ?', "%#{search}%"])#safe
     #where("name LIKE '%#{search}%'")  #not safe
-    #delete_all("name = #{search}")
-    #query = params[:search]
-    #find(:all, :conditions => "name LIKE '%#{search}%'")  #not safe
   end
 end
