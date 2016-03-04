6133ebd45eb1036382bd4cca717918241010111f
smndiaye <serigne.ndiaye@monstar-lab.com>
2016-03-04 10:13:16 +0900

    XSS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   # Disabling CSRF protection
   skip_before_action :verify_authenticity_token
-  skip_before_filter :verify_authenticity_token
+  #skip_before_filter :verify_authenticity_token
   def new
     end
 
